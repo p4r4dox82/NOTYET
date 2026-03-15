9542c1ee886779a7b5c22fc5b311315cd8a70d0f
p4r4dox82 <p4r4dox82@snu.ac.kr>
2026-03-15 19:28:02 +0900

    .

--- 01_Redhorse/clients/src/hooks/useFootPrint.js
+++ 01_Redhorse/clients/src/hooks/useFootPrint.js
@@ -208,7 +208,7 @@ export function useFootPrint(containerRef, canvasRef, normalMapTexture, original
         // 새 노멀맵 저장
         newNormalMapRef.current = newNormalMapTexture;
         // 새 노멀맵과 기존 노멀맵을 blend
-        const blendedTexture = blendNormalMapsWhiteout(originalNormalMapRef.current, newNormalMapTexture, rendererRef.current);
+        const blendedTexture = blendNormalMaps(originalNormalMapRef.current, newNormalMapTexture, rendererRef.current);
         materialRef.current.normalMap = blendedTexture;
         materialRef.current.needsUpdate = true;
         console.log('Normal maps blended successfully');
