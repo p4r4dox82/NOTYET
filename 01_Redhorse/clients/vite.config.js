167f41d4fb7dbc013e9441b5b2c247e195e641e1
p4r4dox82 <p4r4dox82@snu.ac.kr>
2026-03-25 20:58:12 +0900

    [feat] cache 설졍

--- 01_Redhorse/clients/vite.config.js
+++ 01_Redhorse/clients/vite.config.js
@@ -4,10 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      }
+    }
+  },
   server: {
     watch: {
-      usePolling: true, // 폴링 방식 사용 (CPU 사용량이 조금 늘어날 수 있음)
+      usePolling: true,
     },
   },
 })
-
